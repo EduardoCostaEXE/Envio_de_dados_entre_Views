df39b17fd525ac6c4be384f7d1efeb07a50ac76c
unknown <eduardocabral95@gmail.com>
2022-01-20 22:30:33 -0300

    Adicionando Delegate

--- Estudo/Tela2ViewController.swift
+++ Estudo/Tela2ViewController.swift
@@ -7,15 +7,30 @@
 
 import UIKit
 
+protocol tela2ViewControllerDelegate {
+    func devolveTexto(texto: String)
+}
+
 class Tela2ViewController: UIViewController {
+    @IBOutlet weak var txtDeVolta: UITextField!
     
     var nome: String!
 
+    var delegate: tela2ViewControllerDelegate?
+    
     @IBOutlet weak var labelNome: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         labelNome.text = "Olá " + nome
     }
+    @IBAction func btnDeVolta(_ sender: Any) {
+        guard let delegate = delegate, let txtDeVoltaText = txtDeVolta.text else{
+            return
+        }
+        delegate.devolveTexto(texto: txtDeVoltaText)
+        navigationController?.popViewController(animated: true)
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
 
     }
--- Estudo/ViewController.swift
+++ Estudo/ViewController.swift
@@ -8,18 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var textoDeVolta: String = ""
 
     @IBOutlet weak var txtFieldNome: UITextField!
+    @IBOutlet weak var lblDeVolta: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        lblDeVolta.text = textoDeVolta
     }
     @IBAction func btnProximaTela(_ sender: UIButton) {
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         let controllerTela2 = storyboard.instantiateViewController(withIdentifier: "ViewController2") as? Tela2ViewController
         controllerTela2!.nome = txtFieldNome.text
         
+        controllerTela2?.delegate = self
+        
         self.navigationController?.pushViewController(controllerTela2!, animated: true)
         //present(controllerTela2!, animated: true, completion: nil)
     }
 }
 
+extension ViewController: tela2ViewControllerDelegate{
+    func devolveTexto(texto: String) {
+        lblDeVolta.text = texto
+    }
+}
